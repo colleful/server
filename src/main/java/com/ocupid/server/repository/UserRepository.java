1145678475bf06eecf9a3c6be367707cf8d85262
voiciphil <voiciphil@gmail.com>
2020-09-07 19:03:24 +0900

    style: 불필요한 공백 제거

--- src/main/java/com/ocupid/server/repository/UserRepository.java
+++ src/main/java/com/ocupid/server/repository/UserRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByEmail(String email);
 }
