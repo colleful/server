f3376e3911aa9f05109424921175f02d621ee7d6
voiciphil <voiciphil@gmail.com>
2021-01-10 16:08:43 +0900

    test: 매칭 요청 취소 테스트 코드 추가

---
+++ src/test/java/com/colleful/server/domain/matching/service/CancelTest.java
@@ -0,0 +1,48 @@
+package com.colleful.server.domain.matching.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.colleful.server.domain.matching.domain.MatchingRequest;
+import com.colleful.server.domain.matching.repository.MatchingRequestRepository;
+import com.colleful.server.domain.team.domain.Team;
+import com.colleful.server.global.exception.ForbiddenBehaviorException;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CancelTest {
+
+    @InjectMocks
+    private MatchingRequestService matchingRequestService;
+    @Mock
+    private MatchingRequestRepository matchingRequestRepository;
+
+    @Test
+    public void 매칭_취소() {
+        Team team1 = Team.builder().id(1L).matchedTeamId(2L).leaderId(1L).build();
+        Team team2 = Team.builder().id(2L).matchedTeamId(1L).leaderId(2L).build();
+        when(matchingRequestRepository.findById(1L))
+            .thenReturn(Optional.of(new MatchingRequest(team1, team2)));
+
+        matchingRequestService.cancel(1L, 1L);
+
+        verify(matchingRequestRepository).deleteById(1L);
+    }
+
+    @Test
+    public void 권한이_없는_사용자가_매칭_취소() {
+        Team team1 = Team.builder().id(1L).matchedTeamId(2L).leaderId(1L).build();
+        Team team2 = Team.builder().id(2L).matchedTeamId(1L).leaderId(2L).build();
+        when(matchingRequestRepository.findById(1L))
+            .thenReturn(Optional.of(new MatchingRequest(team1, team2)));
+
+        assertThatThrownBy(() -> matchingRequestService.cancel(1L, 3L))
+            .isInstanceOf(ForbiddenBehaviorException.class);
+    }
+}
